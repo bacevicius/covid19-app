a370a80a5e5e083d399d894e4c0667a6d0aea5e6
Vidas <V.Bacevicius@student.tudelft.nl>
2021-02-21 14:09:18 +0200

    Get the covid data, parse and store it in memory

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
 const express = require("express");
 const app = express();
 const unirest = require("unirest");
+const port = 3001;
+
+// Create the global data object where we store the data once when we create the server
+var data;
+// Do this when creating the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+
+  //Do a get request to the required API and get the covid data
+  const request = unirest.get(
+    "https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/json/"
+  );
+
+  request.end(function (response) {
+    if (response.error) throw new Error(response.error);
+
+    // Parse the response json data and save it in a global data object
+    data = JSON.parse(JSON.stringify(response.body));
+    console.log("Data fetched successfully");
+  });
+});
