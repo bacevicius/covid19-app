696e9e2e6c6bf78b4f7aedcacfe679face988dc4
Vidas <V.Bacevicius@student.tudelft.nl>
2021-02-22 20:51:28 +0200

    Backend REST API endpoint

--- server/index.js
+++ server/index.js
@@ -13,16 +13,49 @@ app.get("/allcountries", (req, res) => {
 });
 
 // Rest api endpoint to get covid data by country
-app.get("/", (req, res) => {
+app.get("/country", (req, res) => {
   // Get the required country through the api parameter
-  var country = req.query.country;
+  let country = req.query.country;
 
   // Filter the data array by the country
-  var countryArray = data.filter(function (el) {
+  let countryArray = data.filter(function (el) {
     return el.country === country;
   });
 
-  res.send(countryArray);
+  function constructArray(array) {
+    let cases = [];
+    let deaths = [];
+
+    // Go through the country data array and split it into cases and deaths arrays
+    array.map((item) => {
+      if (item.indicator === "cases") {
+        cases.push(item);
+      } else deaths.push(item);
+    });
+
+   //Modify the cases array so that it contains the weekly case count and death count 
+    cases.map(function(item, index) {
+      item["death_count"] = deaths[index].weekly_count;
+      item["case_count"] = item.weekly_count;
+    });
+
+    // Keep only the essential data we need for the graph
+    let keys_to_keep = ["country", "year_week", "death_count", "case_count"];
+
+    let essentialData = (arr) =>
+      arr.map((o) =>
+        keys_to_keep.reduce((acc, curr) => {
+          acc[curr] = o[curr];
+          return acc;
+        }, {})
+      );
+
+    return essentialData(cases);
+  }
+
+  const finalArray = constructArray(countryArray);
+
+  res.send(finalArray);
 });
 
 // Do this when creating the server
