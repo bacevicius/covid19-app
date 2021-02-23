24de65db29bf846658c2b0177e10bb7f5dba386e
Vidas <V.Bacevicius@student.tudelft.nl>
2021-02-23 14:45:39 +0200

    Add some formatting for the Chart component

--- frontend/src/ChartComponent.js
+++ frontend/src/ChartComponent.js
@@ -1,4 +1,4 @@
-import React, {PureComponent } from "react";
+import React, { PureComponent } from "react";
 import {
   AreaChart,
   CartesianGrid,
@@ -10,25 +10,40 @@ import {
   Area,
 } from "recharts";
 
-
 class Chart extends PureComponent {
-  render() {    
+
+  render() {
     return (
-    <ResponsiveContainer width="80%" height={400}>
-        <AreaChart
-          data={this.props.data}
-          width={1000}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="year_week" interval={8} />
-          <YAxis width={150}/>
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="case_count" name="Cases" stroke="#004fa4" fill="#0477c2" />
-          <Area type="monotone" dataKey="death_count" name="Deaths" stroke="#eb1629" fill="#FF4C4C" />
-        </AreaChart>
-      </ResponsiveContainer>
+      <div style={{ width: "80vw", height: "50vh" }}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={this.props.data}
+            margin={{ top: 5, right: 100, left: 100, bottom: 90 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year_week" interval={this.props.data} angle={-30} tickMargin={40}/>
+            <YAxis />
+            <Tooltip />
+            <Legend  verticalAlign="top" align="right"
+
+ />
+            <Area
+              type="monotone"
+              dataKey="case_count"
+              name="Cases"
+              stroke="#004fa4"
+              fill="#0477c2"
+            />
+            <Area
+              type="monotone"
+              dataKey="death_count"
+              name="Deaths"
+              stroke="#eb1629"
+              fill="#FF4C4C"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     );
   }
 }
