5190650758948e0cd7700cf049bc461a642802ca
Vidas <V.Bacevicius@student.tudelft.nl>
2021-02-24 20:10:08 +0200

    Fixing display of negative cases for some countries

--- frontend/src/ChartComponent.js
+++ frontend/src/ChartComponent.js
@@ -11,21 +11,30 @@ import {
 } from "recharts";
 
 class Chart extends PureComponent {
-
   render() {
     return (
       <div style={{ width: "80vw", height: "50vh" }}>
         <ResponsiveContainer>
           <AreaChart
             data={this.props.data}
-            margin={{ top: 5, right: 50, left: 50, bottom: 50 }}
+            margin={{ top: 0, right: 50, left: 50, bottom: 50 }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="year_week" interval={this.props.data} angle={-30} tickMargin={25} tick={{ fontSize: "1.2rem", width: 250 }} />
-            <YAxis tick={{ fontSize: "1.2rem", width: 250 }}  />
+            <XAxis
+              dataKey="year_week"
+              interval={this.props.data}
+              angle={-30}
+              tickMargin={25}
+              tick={{ fontSize: "1.2rem", width: 250 }}
+            />
+            <YAxis
+              type="number"
+              allowDataOverflow={true}
+              domain={[0, "auto"]}
+              tick={{ fontSize: "1.2rem", width: 250 }}
+            />
             <Tooltip />
-            <Legend  verticalAlign="top" align="right"
- />
+            <Legend verticalAlign="top" align="right" />
             <Area
               type="monotone"
               dataKey="case_count"
